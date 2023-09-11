b2fdb942145171029cc82957a0c7ab69d6a918b4
nurayklc <nuraykilic0101@gmail.com>
2023-09-11 17:02:01 +0300

    Add basic publisher

---
+++ RabbitMQ-Introduction/RabbitMQ.Publisher/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+// Create Connection
+ConnectionFactory connectionFactory = new ConnectionFactory();
+connectionFactory.Uri = new Uri("amqps://rxniewnv:q7meGNULORnRbaxpZqUDUhitAarKxkAC@woodpecker.rmq.cloudamqp.com/rxniewnv");
+
+// Create and Connect Channel
+using IConnection connection = connectionFactory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// Create Queue
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+// Sent to the Queue
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes("merhabaaaaaaaaa" + i);
+    channel.BasicPublish(exchange: "", "example-queue", body: message);
+}
+
+Console.ReadKey();
