5221a7dcd4ca2545f47ba14a77c3dec4edeffafa
nurayklc <nuraykilic0101@gmail.com>
2023-09-11 17:02:27 +0300

    Add basic consumer

---
+++ RabbitMQ-Introduction/RabbitMQ.Consumer/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+// Create Connection
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://rxniewnv:q7meGNULORnRbaxpZqUDUhitAarKxkAC@woodpecker.rmq.cloudamqp.com/rxniewnv");
+
+// Create and Connect Channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+// Create Queue
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+// Read the Message to the Queue
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue:"example-queue", false, consumer: consumer);
+consumer.Received += (sender, e) =>
+{
+    // Queue'ya gelen mesajın işlendiği kısımdır.
+    // e.Body : Queue'daki mesajdır.
+    // e.Body.Span veya e.Body.ToArray() : Queue'daki mesajı byte tipinde getirmektedir.
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.ReadKey();
