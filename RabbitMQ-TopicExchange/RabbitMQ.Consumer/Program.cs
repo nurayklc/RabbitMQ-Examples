c48f5efa7bf973be2932071ee6a363733c0a4941
nurayklc <nuraykilic0101@gmail.com>
2023-09-18 16:36:51 +0300

    Completed topic exchange

---
+++ RabbitMQ-TopicExchange/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://rxniewnv:q7meGNULORnRbaxpZqUDUhitAarKxkAC@woodpecker.rmq.cloudamqp.com/rxniewnv");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "topic-exchange", type: ExchangeType.Topic);
+
+Console.Write("Enter the topic to listen :");
+string topic = Console.ReadLine();
+string queueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(
+    queue: queueName,
+    exchange: "topic-exchange",
+    routingKey: topic);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
---
+++ RabbitMQ-TopicExchange/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://rxniewnv:q7meGNULORnRbaxpZqUDUhitAarKxkAC@woodpecker.rmq.cloudamqp.com/rxniewnv");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "topic-exchange", type: ExchangeType.Topic);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Message {i}");
+    Console.Write("Enter the topic to send the message to: ");
+    string topic = Console.ReadLine();
+    channel.BasicPublish(
+        exchange:"topic-exchange",
+        routingKey: topic,
+        body: message); 
+}
+
+
+Console.Read();
