080b4f2713aac40d9edaaea5d8d8cef8faef214c
nurayklc <nuraykilic0101@gmail.com>
2023-09-18 11:49:39 +0300

    Completed direct exchange project

---
+++ RabbitMQ-DirectExchange/RabbitMQ-Publisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://rxniewnv:q7meGNULORnRbaxpZqUDUhitAarKxkAC@woodpecker.rmq.cloudamqp.com/rxniewnv");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "direct-exchange", type: ExchangeType.Direct);
+
+while (true)
+{
+    Console.Write("Message: ");
+    string message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(
+        exchange: "direct-exchange",
+        routingKey: "direct-queue",
+        body: byteMessage);
+}
+
+Console.Read();  
---
+++ RabbitMQ-DirectExchange/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://rxniewnv:q7meGNULORnRbaxpZqUDUhitAarKxkAC@woodpecker.rmq.cloudamqp.com/rxniewnv");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+// Step 1:
+channel.ExchangeDeclare(exchange: "direct-exchange", type: ExchangeType.Direct);
+
+// Step 2: 
+string queueName = channel.QueueDeclare().QueueName;
+
+// Step 3: 
+channel.QueueBind(
+    queue: queueName,
+    exchange: "direct-exchange",
+    routingKey: "direct-queue");
+
+EventingBasicConsumer consumer = new(channel);
+
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
