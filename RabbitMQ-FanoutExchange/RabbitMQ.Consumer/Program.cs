0b0a723f2542ff79a15be2ff50f717b499e97562
nurayklc <nuraykilic0101@gmail.com>
2023-09-18 15:26:32 +0300

    Completed fanout exchange

---
+++ RabbitMQ-FanoutExchange/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://rxniewnv:q7meGNULORnRbaxpZqUDUhitAarKxkAC@woodpecker.rmq.cloudamqp.com/rxniewnv");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel  = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "fanout-exchange", type: ExchangeType.Fanout);
+
+
+Console.Write("Enter the queue name: ");
+string queueName = Console.ReadLine();
+
+channel.QueueDeclare(
+    queue: queueName,
+    exclusive: false);
+
+channel.QueueBind(
+    queue: queueName,
+    exchange: "fanout-exchange",
+    routingKey: String.Empty);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    consumer: consumer,
+    autoAck: true
+    );
+
+consumer.Received += (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));    
+};
+
+
+Console.Read();
---
+++ RabbitMQ-FanoutExchange/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://rxniewnv:q7meGNULORnRbaxpZqUDUhitAarKxkAC@woodpecker.rmq.cloudamqp.com/rxniewnv");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "fanout-exchange", type: ExchangeType.Fanout);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Message {i}");
+    channel.BasicPublish(
+        exchange: "fanout-exchange", 
+        routingKey: String.Empty,
+        body: message);
+}
+
+Console.Read();
