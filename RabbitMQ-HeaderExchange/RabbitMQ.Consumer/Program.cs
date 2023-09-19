eb24bb334dd54381fd8680666ccb86f4e9b1f146
nurayklc <nuraykilic0101@gmail.com>
2023-09-19 10:52:52 +0300

    Completed headers exchange example

--- RabbitMQ-HeaderExchange/RabbitMQ.Consumer/Program.cs
+++ RabbitMQ-HeaderExchange/RabbitMQ.Consumer/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://rxniewnv:q7meGNULORnRbaxpZqUDUhitAarKxkAC@woodpecker.rmq.cloudamqp.com/rxniewnv");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "headers-exchange", type: ExchangeType.Headers);
+
+Console.Write("Enter the header value: ");
+string value = Console.ReadLine();
+string queueName = channel.QueueDeclare().QueueName;
+
+channel.QueueBind(
+    queue: queueName,
+    exchange: "headers-exchange",
+    routingKey: String.Empty,
+    new Dictionary<string, object>
+    {
+        ["x-match"] = "all",
+        ["no"] = value
+    });
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
--- RabbitMQ-HeaderExchange/RabbitMQ.Publisher/Program.cs
+++ RabbitMQ-HeaderExchange/RabbitMQ.Publisher/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://rxniewnv:q7meGNULORnRbaxpZqUDUhitAarKxkAC@woodpecker.rmq.cloudamqp.com/rxniewnv");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "headers-exchange", type: ExchangeType.Headers);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Hello {i}");
+    Console.Write("Enter the header value: ");
+    string value = Console.ReadLine();
+
+    IBasicProperties properties = channel.CreateBasicProperties();
+    properties.Headers = new Dictionary<string, object>
+    {
+        ["no"] = value
+    };
+
+    channel.BasicPublish(
+        exchange: "headers-exchange",
+        routingKey: String.Empty,
+        body: message,
+        basicProperties: properties
+        );
+}
+
+Console.Read();
