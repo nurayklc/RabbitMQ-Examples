8a6d9b3f303abbd33c15c94c492e7d9f760f26c9
nurayklc <nuraykilic0101@gmail.com>
2023-09-23 14:54:13 +0300

    Completed Mass Transit wit console app

---
+++ RabbitMQ-ESB-MassTransit/RabbitMQ.ESB.MassTransit.Consumer/RabbitMQ.ESB.MassTransit.Consumer/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using MassTransit;
+using RabbitMQ.ESB.MassTransit.Consumer.Consumers;
+
+string rabbitMQUri = "amqps://rxniewnv:q7meGNULORnRbaxpZqUDUhitAarKxkAC@woodpecker.rmq.cloudamqp.com/rxniewnv";
+
+string queueName = "mass-transit";
+
+IBusControl bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+{
+    factory.Host(rabbitMQUri);
+    factory.ReceiveEndpoint(queueName, endpoint =>
+    {
+        endpoint.Consumer<MessageConsumer>();
+    });
+});
+
+await bus.StartAsync();
+
+Console.Read();
---
+++ RabbitMQ-ESB-MassTransit/RabbitMQ.ESB.MassTransit.Publisher/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using MassTransit;
+using RabbitMQ.ESB.MassTransit.Shared.Messages;
+
+string rabbitMQUri = "amqps://rxniewnv:q7meGNULORnRbaxpZqUDUhitAarKxkAC@woodpecker.rmq.cloudamqp.com/rxniewnv";
+
+string queueName = "mass-transit";
+
+IBusControl bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+{
+    factory.Host(rabbitMQUri);
+});
+
+ISendEndpoint sendEndpoint = await bus.GetSendEndpoint(
+    new($"{rabbitMQUri}/{queueName}"));
+
+Console.Write("Send to message: ");
+string message = Console.ReadLine();
+
+await sendEndpoint.Send<IMessage>(new Message()
+{
+    Text = message
+});
+
+Console.Read();
